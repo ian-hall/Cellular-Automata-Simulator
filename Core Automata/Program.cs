fee62a44aa8dee0e12cd75e120bc37e33dd4547c
ian <ian-hall@users.noreply.github.com>
2018-04-15 11:25:21 -0700

    Add project files.

---
+++ Core Automata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core_Automata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
